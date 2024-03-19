ca5621b9398110a338284040ff59763de61ecf20
kian.p.m.007@gmail.com <kian.p.m.007@gmail.com>
2024-03-19 11:15:40 -0500

    feat: Add new button styles and components
    Added new button styles using Tailwind CSS variants in button.svelte.
    Added a new input component with custom styles in input.svelte.
    Added a new icon component in icon.svg
    Updated the styles of the generic container in container.svelte.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	import { Button } from '$ui/button';
+	import * as Card from '$ui/card';
+	import { Input } from '$ui/input';
+	import { Label } from '$ui/label';
+</script>
+
+<!-- <div class="container pt-10">
+	<div class="max-w-[300px] space-y-2">
+		<div>
+			<h1 class="text-5xl font-semibold">
+				<span>Blue</span><span class="text-blue-600">Q</span>
+			</h1>
+			<p>Effective communication to the masses.</p>
+		</div>
+		<div class="space-y-1">
+			<Label>Email</Label>
+			<Input placeholder="example@email.com" type="email" />
+            <Button>Submit</Button>
+		</div>
+	</div>
+</div> -->
+
+<div class="flex">
+	<div class="flex h-svh w-[50%] items-center justify-center">
+		<div class="w-[350px]">
+			<h1 class="text-3xl font-semibold">
+				<span>Blue</span><span class="text-blue-600">Q</span>
+			</h1>
+			<p class="text-muted-foreground">
+				Transform anticipation into engagement, from waitlist to launch.
+			</p>
+		</div>
+	</div>
+	<div class="flex h-svh w-[50%] items-center justify-center bg-gray-100">
+        <Card.Root class="w-[350px]">
+            <Card.Header>
+                <Card.Title>
+                    Early access
+                </Card.Title>
+                <Card.Description>
+                    Get notified when we're ready.
+                </Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <Label>Email</Label>
+                <Input placeholder="example@email.com" type="email"></Input>
+            </Card.Content>
+            <Card.Footer class="flex justify-end">
+                <Button>
+                    Submit
+                </Button>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+</div>

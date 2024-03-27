0f882f073b28678f8bf20ec7d38870cd3c27b302
Kian McKenna <kian.p.m.007@gmail.com>
2024-03-26 21:31:13 -0500

    feat: Add projects section to home page
    Added a new projects section to the home page displaying project cards with titles and types. Each project card includes an icon based on the project type. New projects component was created to handle the rendering of project cards.

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,14 +1,45 @@
 <script>
 	import { ChevronDown, Mails, Rocket } from 'lucide-svelte';
 
+	import Projects from "./projects.svelte";
+
 	import { Button } from '$ui/button';
 	import * as Card from '$ui/card';
 	import * as Dropdown from '$ui/dropdown-menu';
 
 	export let data;
-	let { projects } = data;
-	$: ({ projects } = data);
+	// let { projects } = data;
+	// $: ({ projects } = data);
+
+	let projects = [
+		{
+			title: "Vercel",
+			type: "launch"
+		},
+		{
+			title: "Arc",
+			type: "mailing"
+		},
+		{
+			title: "Vesturio",
+			type: "launch",
+		},
+		{
+			title: "Vercel",
+			type: "launch"
+		},
+		{
+			title: "Arc",
+			type: "mailing"
+		},
+	]
 </script>
+
+<svelte:head>
+	<title>
+		Home - BlueQ
+	</title>
+</svelte:head>
 
 <div class="container mx-auto p-2">
 	<nav class="flex items-center justify-between pb-2">
@@ -38,7 +69,7 @@
 			</Dropdown.Root>
 		</div>
 	</nav>
-	<div class="grid md:grid-cols-2 gap-2 pb-2">
+	<div class="grid gap-2 pb-2 md:grid-cols-2">
 		<Card.Root>
 			<Card.Header>
 				<Card.Description>Subscribers (24hrs)</Card.Description>
@@ -54,13 +85,6 @@
 	</div>
 	<div>
 		<h1 class="pb-2 text-lg font-semibold">Projects</h1>
-		<div class="grid md:grid-cols-3 gap-2">
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>Project</Card.Title>
-					<Card.Description>Project description</Card.Description>
-				</Card.Header>
-			</Card.Root>
-		</div>
+		<Projects {projects} />
 	</div>
 </div>
---
+++ src/routes/home/projects.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Rocket, Mails } from 'lucide-svelte';
+	import * as Card from '$ui/card';
+	export let projects;
+</script>
+
+<div class="grid gap-2 md:grid-cols-3">
+	{#each projects as project}
+		<a href={`/${project.id}`}>
+			<Card.Root>
+				<Card.Header>
+					<div class="flex space-x-1">
+						<svelte:component this={project.type === 'launch' ? Rocket : Mails} size={16} />
+						<Card.Title>{project.title}</Card.Title>
+					</div>
+					<Card.Description>0 subscribers</Card.Description>
+				</Card.Header>
+			</Card.Root>
+		</a>
+	{/each}
+</div>

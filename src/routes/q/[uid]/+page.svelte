29f643cef5734eb14aa75fdc3fa72117fd76404d
Kian McKenna <kian.p.m.007@gmail.com>
2024-03-28 12:22:38 -0500

    feat: Update project URLs to use current page origin
    Updated project URLs in the projects.svelte file to use the current page's origin instead of hardcoding 'blueq.app'. Also added a new page for displaying project launch details.

---
+++ src/routes/q/[uid]/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { page } from '$app/stores';
+
+	import { SplitScreen } from '$lib/components/layouts';
+
+	import { Button } from '$ui/button';
+	import * as Card from '$ui/card';
+
+	export let data;
+	const { project } = data;
+</script>
+
+<svelte:head>
+	<title>
+		{project.title} - Launch Page
+	</title>
+</svelte:head>
+
+<SplitScreen>
+	<div slot="left">
+		<h1 class="text-3xl font-semibold">
+			<span>{project.title}</span>
+		</h1>
+		<p class="text-muted-foreground">
+			{project.caption}
+		</p>
+	</div>
+	<div slot="right">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Launch list</Card.Title>
+				<Card.Description>
+					Receive launch updates. Sign up for progress notifications.
+				</Card.Description>
+			</Card.Header>
+		</Card.Root>
+	</div>
+</SplitScreen>

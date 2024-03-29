c3fa1dd359792d690c73aa691b0f10ee5b40335c
Kian McKenna <kian.p.m.007@gmail.com>
2024-03-29 18:42:44 -0500

    feat: Improve OG page layout and rendering
    Refactor OG page layout in og.svelte to center align text elements.
    Update server.js to handle optional project title and caption values.
    Remove unnecessary console log in render function.

--- src/routes/q/[uid]/og/og.svelte
+++ src/routes/q/[uid]/og/og.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex h-full items-center justify-center bg-white">
 	<div class="flex flex-col items-center">
-		<h1 class="text-9xl font-semibold">{title}</h1>
-		<p class="text-5xl text-[#64748b]">{caption}</p>
+		<h1 class="text-center text-9xl font-semibold">{title}</h1>
+		<p class="text-center text-5xl text-[#64748b]">{caption}</p>
 	</div>
 </div>

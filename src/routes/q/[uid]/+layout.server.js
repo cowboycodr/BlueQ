7ca1ffd44120e18d542fd67236c3a4edc45fda2a
Kian McKenna <kian.p.m.007@gmail.com>
2024-03-29 14:10:50 -0500

    feat: Add OG image generation for project launch pages
    - Added script in og.svelte to display project title and caption in OG image.
    - Removed unnecessary variables in page.server.js load function.
    - Added layout.server.js to fetch project data for layout rendering.
    - Implemented server-side rendering in og/+server.js to generate image.
    - Updated dependencies: @resvg/resvg-js, satori, satori-html.

---
+++ src/routes/q/[uid]/+layout.server.js
@@ -0,0 +1,11 @@
+export const load = async ({ params, locals: { supabase } }) => {
+    const { uid } = params;
+
+    const { data: project, error } = await supabase
+        .from("projects")
+        .select("*")
+        .eq("short_code", uid)
+        .single();
+
+    return { project };
+}
--- src/routes/q/[uid]/+page.svelte
+++ src/routes/q/[uid]/+page.svelte
@@ -11,6 +11,7 @@
 	<title>
 		{project.title} - Launch Page
 	</title>
+	
 </svelte:head>
 
 <SplitScreen>

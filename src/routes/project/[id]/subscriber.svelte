d8101dab40e586751bccac49bc0f88d90c72d9fb
cowboycodr <kian.p.m.007@gmail.com>
2024-03-30 13:51:48 -0500

    feat(subscriber): Add individual subscriber component and master selection checkbox
    Added a new individual subscriber component to display subscriber details and the master selection checkbox. The master selection checkbox allows selecting or deselecting all subscribers at once. This improves the user experience by providing more control over subscriber selection.

---
+++ src/routes/project/[id]/subscriber.svelte
@@ -0,0 +1,26 @@
+<script>
+    import moment from "moment";
+
+    import * as Table from '$ui/table';
+    import { Checkbox } from '$ui/checkbox';
+
+    export let subscriber;
+    export let selected = false;
+
+    $: console.log({ selected })
+</script>
+
+<Table.Row>
+    <Table.Cell>
+        <Checkbox bind:checked={selected} />
+    </Table.Cell>
+    <Table.Cell>
+        {subscriber.email}
+    </Table.Cell>
+    <Table.Cell>
+        {subscriber.location}
+    </Table.Cell>
+    <Table.Cell>
+        {moment(subscriber.created_at).format("MM/DD/YY")}
+    </Table.Cell>
+</Table.Row>

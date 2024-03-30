71b913afe395e73ac8c57e96e3c4a8fc65f8d70e
cowboycodr <kian.p.m.007@gmail.com>
2024-03-30 17:02:38 -0500

    refactor: Increase max width of caption in og image container
    Updated the max width of the caption in the og image container to 95% to
    improve readability and visual appeal.

--- src/routes/q/[uid]/og/og.svelte
+++ src/routes/q/[uid]/og/og.svelte
@@ -6,6 +6,6 @@
 <div class="flex h-full items-center justify-center bg-white">
 	<div class="flex flex-col items-center">
 		<h1 class="text-center text-9xl font-semibold">{title}</h1>
-		<p class="text-center text-5xl text-[#64748b]">{caption}</p>
+		<p class="max-w-[95%] text-center text-5xl text-[#64748b]">{caption}</p>
 	</div>
 </div>

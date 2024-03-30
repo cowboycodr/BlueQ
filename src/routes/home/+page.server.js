41b3b4b529ee73301bb3a1214c9676b5ea07eb97
cowboycodr <kian.p.m.007@gmail.com>
2024-03-30 16:57:08 -0500

    refactor: Remove redundant code and add newProject action for creating projects

--- src/routes/home/+page.server.js
+++ src/routes/home/+page.server.js
@@ -6,72 +6,6 @@ import { generateUniqueTag } from '$lib/server/utils/tag-utils.js';
 
 const uid = new ShortUniqueId({ length: 8 });
 
-// export const load = async ({ locals: { supabase, getSession } }) => {
-//     const session = await getSession();
-
-//     const projects = await (async () => {
-//         const { data, error } = await supabase
-//             .from("projects")
-//             .select("*")
-//             .eq("owner_id", session.user.id)
-//             .order("created_at", { ascending: false });
-
-//         return data;
-//     })();
-
-//     const launchPageForm = await superValidate(zod(launchPageFormSchema));
-
-//     return {
-//         projects,
-//         launchPageForm,
-//     }
-// }
-
-// export const actions = {
-//     newProject: async ({ request, locals: { supabase, getUser } }) => {
-//         const user = await getUser();
-
-//         const form = await superValidate(request, zod(launchPageFormSchema));
-
-//         if (!form.valid) {
-//             return fail(400, {
-//                 launchPageForm: form,
-//             });
-//         }
-
-//         const title = form.data.title;
-//         const caption = form.data.caption;
-
-//         const tag = await generateUniqueTag(title, supabase);
-//         const shortCode = uid.rnd();
-
-//         const { data, error } = await supabase
-//             .from("projects")
-//             .insert([
-//                 {
-//                     title,
-//                     caption,
-//                     tag,
-//                     type: "launch",
-//                     owner_id: user.id,
-//                     short_code: shortCode,
-//                 },
-//             ]);
-
-//         if (error) {
-//             console.error(error);
-
-//             return fail(500, {
-//                 launchPageForm: form,
-//             });
-//         }
-
-//         return {
-//             launchPageForm: form,
-//         };
-//     },
-// };
-
 export const load = async ({ locals: { supabase, getSession } }) => {
     const session = await getSession();
 
@@ -109,3 +43,48 @@ export const load = async ({ locals: { supabase, getSession } }) => {
         launchPageForm,
     }
 }
+
+export const actions = {
+    newProject: async ({ request, locals: { supabase, getUser } }) => {
+        const user = await getUser();
+
+        const form = await superValidate(request, zod(launchPageFormSchema));
+
+        if (!form.valid) {
+            return fail(400, {
+                launchPageForm: form,
+            });
+        }
+
+        const title = form.data.title;
+        const caption = form.data.caption;
+
+        const tag = await generateUniqueTag(title, supabase);
+        const shortCode = uid.rnd();
+
+        const { data, error } = await supabase
+            .from("projects")
+            .insert([
+                {
+                    title,
+                    caption,
+                    tag,
+                    type: "launch",
+                    owner_id: user.id,
+                    short_code: shortCode,
+                },
+            ]);
+
+        if (error) {
+            console.error(error);
+
+            return fail(500, {
+                launchPageForm: form,
+            });
+        }
+
+        return {
+            launchPageForm: form,
+        };
+    },
+};

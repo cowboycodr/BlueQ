52d026ca032aa6638a587dbb6f4abc6927b8302e
cowboycodr <kian.p.m.007@gmail.com>
2024-03-30 15:58:28 -0500

    feat: Display the number of subscribers in the last 24 hours in project page
    Added functionality to filter subscribers based on creation date within the last
    24 hours. Updated subscriber count display accordingly. Updated UI for selecting
    and deselecting subscribers.

--- src/routes/project/[id]/subscribers.svelte
+++ src/routes/project/[id]/subscribers.svelte
@@ -1,16 +1,32 @@
 <script>
-    import moment from "moment";
+	import moment from 'moment';
 
 	import { Input } from '$ui/input';
 	import { Checkbox } from '$ui/checkbox';
 	import * as Table from '$ui/table';
 
-    import Subscriber from "./subscriber.svelte";
+	import Subscriber from './subscriber.svelte';
 
 	export let project;
 	export let subscribers;
 
-    let masterSelected = false;
+	let masterSelected = false;
+    let selected = [];
+
+    function handleSelect(ev) {
+        const { subscriber } = ev.detail;
+
+        selected = [subscriber.id, ...selected];
+    }
+
+    function handleDeselect(ev) {
+        const { subscriber } = ev.detail;
+
+        // Filter out the deselected subscriber's ID from the 'selected' array
+        selected = selected.filter(id => id !== subscriber.id);
+    }
+
+    $: console.log({ selected });
 </script>
 
 <Table.Root>
@@ -22,14 +38,16 @@
 			<Table.Head class="flex items-center">
 				<Checkbox bind:checked={masterSelected} />
 			</Table.Head>
-			<Table.Head>Email</Table.Head>
+			<Table.Head>
+                <Input placeholder="Email" />
+            </Table.Head>
 			<Table.Head>Location</Table.Head>
 			<Table.Head>Date subscribed</Table.Head>
 		</Table.Row>
 	</Table.Header>
-    <Table.Body>
-        {#each subscribers as subscriber}
-            <Subscriber selected={masterSelected} {subscriber} />
-        {/each}
-    </Table.Body>
+	<Table.Body>
+		{#each subscribers as subscriber}
+			<Subscriber {subscriber} selected={masterSelected} on:select={handleSelect} on:deselect={handleDeselect} />
+		{/each}
+	</Table.Body>
 </Table.Root>

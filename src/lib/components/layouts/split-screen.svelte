25655a8300334892b754254ae14af690b1edad01
cowboycodr <kian.p.m.007@gmail.com>
2024-03-31 15:44:31 -0500

    refactor: Update split-screen layout to use full height on larger screens
    - Adjusted the height of elements in split-screen layout to use full height on
      larger screens for better visual consistency and responsiveness. 
    - Also made minor style adjustments to the footer position in split-screen
      layout for improved alignment.

--- src/lib/components/layouts/split-screen.svelte
+++ src/lib/components/layouts/split-screen.svelte
@@ -5,12 +5,14 @@
 <div
 	class="mx-auto flex min-h-svh w-full max-w-sm flex-col justify-center md:max-w-none md:flex-row"
 >
-	<div class="flex items-center justify-center md:h-svh md:w-[50%]">
+	<div class="flex items-center justify-center md:h-full md:min-h-svh md:w-[50%]">
 		<div class="w-full p-2 md:w-[350px] md:p-0">
 			<slot name="left" />
 		</div>
 	</div>
-	<div class="flex items-center justify-center p-2 md:h-svh md:w-[50%] md:bg-gray-100 md:p-0">
+	<div
+		class="flex items-center justify-center p-2 md:h-full md:min-h-svh md:w-[50%] md:bg-gray-100 md:p-0"
+	>
 		<div class="w-full md:w-[350px]">
 			<slot name="right" />
 		</div>
@@ -18,8 +20,8 @@
 </div>
 
 {#if SLOTS?.footer}
-	<div class="absolute bottom-5 flex w-full justify-center">
-		<p class="rounded-lg bg-white p-1 text-muted-foreground">
+	<div class="bottom-5 flex w-full justify-center md:absolute">
+		<p class="rounded-lg p-1 text-muted-foreground">
 			<slot name="footer" />
 		</p>
 	</div>

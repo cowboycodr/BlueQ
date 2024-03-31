1e97bce9661877fc385c4834cca7762b5a923f40
cowboycodr <kian.p.m.007@gmail.com>
2024-03-31 16:23:52 -0500

    feat: Add delete option to subscriber row in project page
    Add a delete option to the subscriber row in the project page by including
    the Trash icon and implementing the delete functionality. This allows users
    to easily delete subscribers directly from the table.

--- src/routes/project/[id]/subscriber.svelte
+++ src/routes/project/[id]/subscriber.svelte
@@ -3,11 +3,14 @@
 	const dispatch = createEventDispatcher();
 
 	import moment from 'moment';
-	import { MoreHorizontal } from 'lucide-svelte';
+	import { MoreHorizontal, Trash, Copy } from 'lucide-svelte';
 
 	import { Button } from '$ui/button';
+	import * as Dropdown from '$ui/dropdown-menu';
 	import { Checkbox } from '$ui/checkbox';
 	import * as Table from '$ui/table';
+
+	import { copy } from '$utils';
 
 	export let subscriber;
 	export let selected = false;
@@ -24,7 +27,7 @@
 	$: dispatchSelectState(selected);
 </script>
 
-<Table.Row class="{ selected ? "bg-gray-100" : "" }">
+<Table.Row class={selected ? 'bg-gray-100' : ''}>
 	<Table.Cell>
 		<Checkbox bind:checked={selected} />
 	</Table.Cell>
@@ -39,9 +42,31 @@
 	</Table.Cell>
 	<Table.Cell>
 		<div class="flex items-center">
-			<Button variant="ghost">
-				<MoreHorizontal size="16" />
-			</Button>
+			<Dropdown.Root>
+				<Dropdown.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="ghost">
+						<MoreHorizontal size="16" />
+					</Button>
+				</Dropdown.Trigger>
+				<Dropdown.Content>
+					<Dropdown.Label>
+						{subscriber.email}
+					</Dropdown.Label>
+					<Dropdown.Separator />
+					<Dropdown.Group>
+						<Dropdown.Item>
+							<div class="flex space-x-1" use:copy={subscriber.email}>
+								<Copy size={16} />
+								<span> Copy email </span>
+							</div>
+						</Dropdown.Item>
+						<Dropdown.Item class="flex space-x-1">
+							<Trash size={16} class="text-destructive" />
+							<span class="text-destructive"> Delete </span>
+						</Dropdown.Item>
+					</Dropdown.Group>
+				</Dropdown.Content>
+			</Dropdown.Root>
 		</div>
 	</Table.Cell>
 </Table.Row>

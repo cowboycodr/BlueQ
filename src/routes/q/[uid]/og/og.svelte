a291be323f586ccb399fd4f665d5816cba2b80d8
cowboycodr <kian.p.m.007@gmail.com>
2024-04-01 17:11:25 -0500

    feat: Add "Powered by BlueQ" attribution in og.svelte
    Add a new div at the bottom of the og.svelte file to display the text
    "Powered by BlueQ." This attribution provides credit to the tool used for
    generating the OG image.

--- src/routes/q/[uid]/og/og.svelte
+++ src/routes/q/[uid]/og/og.svelte
@@ -8,4 +8,7 @@
 		<h1 class="text-center text-9xl font-semibold">{title}</h1>
 		<p class="max-w-[95%] text-center text-5xl text-[#64748b]">{caption}</p>
 	</div>
+	<div class="absolute bottom-5 flex text-center">
+		<p class="text-[#64748b] text-2xl">Powered by BlueQ.</p>
+	</div>
 </div>
